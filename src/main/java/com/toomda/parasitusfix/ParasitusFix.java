508e4f0e7500b0897ccd92beab22e98e878bfb32
suichanwa <augustinnegura746@gmail.com>
2026-02-09 23:57:20 +0200

    fixed zombie spawn in end

--- src/main/java/com/toomda/parasitusfix/ParasitusFix.java
+++ src/main/java/com/toomda/parasitusfix/ParasitusFix.java
@@ -12,15 +12,17 @@ import com.toomda.parasitusfix.sevendaystomine.EnchantItemFix;
 import com.toomda.parasitusfix.sevendaystomine.SevenDaysBlockPatches;
 import com.toomda.parasitusfix.sevendaystomine.SevenDaysChanceConfigGuard;
 import com.toomda.parasitusfix.sevendaystomine.SevenDaysDamagePatches;
-import com.toomda.parasitusfix.sevendaystomine.ZombieSpawnFix;
 import com.toomda.parasitusfix.techguns.TechgunsAttackHelicopterTargetFix;
 import com.toomda.parasitusfix.techguns.TechgunsAttackHelicopterNoDespawn;
 import com.toomda.parasitusfix.techguns.TechgunsSoldierZombieTargetFix;
 import com.toomda.parasitusfix.techguns.TechgunsGrinderDurabilityFix;
 import com.toomda.parasitusfix.techguns.TechgunsBlockHardnessCap;
 import com.toomda.parasitusfix.buildcraft.QuartzKinesisPipeCapRemoval;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -28,14 +30,21 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Logger;
+import techguns.entities.npcs.ZombieFarmer;
+import techguns.entities.npcs.ZombieMiner;
+import techguns.entities.npcs.ZombiePigmanSoldier;
+import techguns.entities.npcs.ZombiePoliceman;
+import techguns.entities.npcs.ZombieSoldier;
 
 @Mod(modid = ParasitusFix.MODID, name = ParasitusFix.NAME, version = ParasitusFix.VERSION, dependencies = "required-after:sevendaystomine")
 public class ParasitusFix
 {
     public static final String MODID = "parasitusfix";
     public static final String NAME = "ParasitusFix";
-    public static final String VERSION = "1.1.02";
+    public static final String VERSION = "1.3";
 
     private static Logger logger;
 
@@ -58,7 +67,6 @@ public class ParasitusFix
             MinecraftForge.EVENT_BUS.register(new BleedDamageLimiter());
             MinecraftForge.EVENT_BUS.register(new BleedEffectClamp());
             MinecraftForge.EVENT_BUS.register(new BandageInstantUse());
-            MinecraftForge.EVENT_BUS.register(new ZombieSpawnFix());
             MinecraftForge.EVENT_BUS.register(new BarbedWireDurabilityFix());
             MinecraftForge.EVENT_BUS.register(new EnchantItemFix());
         }
@@ -66,6 +74,7 @@ public class ParasitusFix
             MinecraftForge.EVENT_BUS.register(new TechgunsAttackHelicopterTargetFix());
             MinecraftForge.EVENT_BUS.register(new TechgunsAttackHelicopterNoDespawn());
             MinecraftForge.EVENT_BUS.register(new TechgunsSoldierZombieTargetFix());
+            MinecraftForge.EVENT_BUS.register(new TechgunsZombieEndSpawnRestrict());
         }
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
@@ -89,6 +98,39 @@ public class ParasitusFix
         event.registerServerCommand(new ParasitusFixCommand());
         logger.info("ParasitusFix commands registered");
     }
-}
 
+    private static final class TechgunsZombieEndSpawnRestrict {
+        private static final int END_DIMENSION_ID = 1;
+
+        @SubscribeEvent
+        public void onCheckSpawn(LivingSpawnEvent.CheckSpawn event) {
+            if (!isTechgunsZombie(event.getEntity())) return;
+
+            World world = event.getWorld();
+            if (world.provider.getDimension() == END_DIMENSION_ID) {
+                event.setResult(Event.Result.DENY);
+                ParasitusFix.getLogger().debug("Blocked Techguns zombie spawn in The End");
+            }
+        }
+
+        @SubscribeEvent
+        public void onSpecialSpawn(LivingSpawnEvent.SpecialSpawn event) {
+            if (!isTechgunsZombie(event.getEntity())) return;
+
+            World world = event.getWorld();
+            if (world.provider.getDimension() == END_DIMENSION_ID) {
+                event.setCanceled(true);
+                ParasitusFix.getLogger().debug("Blocked Techguns zombie special spawn in The End");
+            }
+        }
+
+        private static boolean isTechgunsZombie(Entity entity) {
+            return entity instanceof ZombieSoldier
+                || entity instanceof ZombiePoliceman
+                || entity instanceof ZombieFarmer
+                || entity instanceof ZombieMiner
+                || entity instanceof ZombiePigmanSoldier;
+        }
+    }
+}
 
