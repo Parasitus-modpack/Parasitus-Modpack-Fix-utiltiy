4ff5f95082085ebb777b5caea45f2e842ff9b9d1
suichanwa <augustinnegura746@gmail.com>
2026-03-23 04:11:43 +0200

    fixed little thingie

--- src/main/java/com/toomda/parasitusfix/mixin/ParasitusFixMixinLoader.java
+++ src/main/java/com/toomda/parasitusfix/mixin/ParasitusFixMixinLoader.java
@@ -5,7 +5,6 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import zone.rong.mixinbooter.IEarlyMixinLoader;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
